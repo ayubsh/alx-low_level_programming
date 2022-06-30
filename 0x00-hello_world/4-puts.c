b384b6d6ee9dcbb6cbd20242f72daf3c274b6523
ayubsh <ibrahimayubsheikh@gmail.com>
2022-06-30 05:04:45 -0700

    prints phrase using puts func

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - Prints the phrase Programming is like building a multilingual puzzle
+ * using the puts function
+ * Return: Always 0 (Success)
+ * /
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return ( 0 );
+}
