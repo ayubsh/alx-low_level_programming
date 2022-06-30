df1c5e4af0c707b7b6128386303071a05b53949c
ayubsh <ibrahimayubsheikh@gmail.com>
2022-06-30 06:33:17 -0700

    Prints into the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints into the standard error
+ * Return: returns 1 (error)
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
