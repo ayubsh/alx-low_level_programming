1f2df694ac56de3a6b52544d081d9b2552230483
ayubsh <ibrahimayubsheikh@gmail.com>
2022-07-01 11:10:33 -0700

    prints base16 numbers

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,14 +7,14 @@ int main(void)
 {
 	char a;
 
-	for(a = '0'; a <= '9'; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		putchar(a);
+		putchar (a);
 	}
 	for (a = 'a'; a <= 'f'; a++)
 	{
-		putchar(a);
+		putchar (a);
 	}
-	putchar('\n');
+	putchar ('\n');
 	return (0);
 }
