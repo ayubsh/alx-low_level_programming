2b9a9d7ee78a85e94ffdadbe689d98e54ad2be94
ayubsh <ibrahimayubsheikh@gmail.com>
2022-07-01 10:53:02 -0700

    number 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print single digit number in 10
+ * Return: returns 0 always (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
