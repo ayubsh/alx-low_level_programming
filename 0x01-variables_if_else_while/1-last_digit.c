a0d96c59852c454a7986490c209a808a160753ca
ayubsh <ibrahimayubsheikh@gmail.com>
2022-07-01 08:46:55 -0700

    Checks last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - checks weather last digit of is
+ * greater than 5
+ * equals 0
+ * or less than 6 and not equal to 0
+ * Return: returns 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n%10 > 5)
+	{
+		printf("is %d and is greater than 5\n", n);
+	}
+	else if (n%10 == 0)
+	{
+		printf("is %d and is 0\n", n);
+	}
+	else if (n%10 < 6 && n%10 != 0)
+	{
+		printf("is %d and is less than 6 and not 0\n", n);
+	}
+	return (0);
+}
