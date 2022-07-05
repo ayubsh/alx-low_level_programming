130242637b580538da9ddc4b222b508fedb29ddf
ayubsh <ibrahimayubsheikh@gmail.com>
2022-07-05 10:46:21 -0700

    Prints the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,9 +10,8 @@ int print_last_digit(int c)
 	int last_digit;
 
 	last_digit = c % 10;
-
-	printf("%d", last_digit);
-	printf("\n");
-
+	if (c < 0)
+		last_digit = last_digit * -1;
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
