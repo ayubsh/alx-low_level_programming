4f5446aa89426f1ee7171a69594bcae46f4e9d44
ayubsh <ibrahimayubsheikh@gmail.com>
2022-07-05 10:51:54 -0700

    adds two int

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - adds two integers
+ * @a: fist number
+ * @b: second number
+ * Return: returns the sum of a and b
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
