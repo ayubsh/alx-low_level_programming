af8dbb570b575f9a5f6e88b383f87d457e7d673f
ayubsh <ibrahimayubsheikh@gmail.com>
2022-07-05 10:58:47 -0700

    print numbers from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+/**
+ * print_to_98 - print number from n to 98
+ * @n: starting point
+ * Return: returns void, nothing
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d, ", i);
+	}
+}
