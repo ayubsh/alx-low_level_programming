5fbd4c5d555a22c169b94630819f16a52c534af6
ayubsh <ibrahimayubsheikh@gmail.com>
2022-07-05 12:31:45 -0700

    Prints the times table of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * times_table - printd the times table of 9
+ * Return: returns void, nothing
+ */
+void times_table(void)
+{
+	int a, b, hold;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			hold = a * b;
+			if (b == 0)
+			{
+				_putchar('0' + hold);
+			}
+			else if (hold < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + hold);
+			}
+			else
+			{
+				_putchar('0' + hold / 10);
+				_putchar('0' + hold % 10);
+			}
+
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
