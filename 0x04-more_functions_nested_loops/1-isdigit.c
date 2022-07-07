c2bdc189a7a5aef6604b90a374c7d16bd377acd9
ayubsh <ibrahimayubsheikh@gmail.com>
2022-07-07 09:53:46 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+/**
+ * _isdigit - checks weather input is digit or not
+ * @c: input to be checked
+ * Return: 1 when c is digit otherwise 0
+ */
+int _isdigit(int c)
+{
+	if(c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
