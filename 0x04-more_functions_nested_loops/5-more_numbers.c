386bebceac9e3d62de7a4e52c6befa27f44954ee
ayubsh <ibrahimayubsheikh@gmail.com>
2022-07-07 11:42:24 -0700

    numbers 0-14 10times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0-14 10 times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = '0'; i <= '10'; i++ )
+	{
+		for (i = '0'; i <= '14'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
