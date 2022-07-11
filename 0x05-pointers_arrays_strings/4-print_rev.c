939786ecea39b6457dbd27878fe783796e4520dd
ayubsh <ibrahimayubsheikh@gmail.com>
2022-07-11 07:06:16 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_rev - prints the reverse of the input
+ * @s: input to be reversed
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len;
+
+	len = strlen(s);
+
+	while (i >= 0)
+	{
+		putchar(s[i]);
+		i--;
+	}
+	putchar('\n');
+}
