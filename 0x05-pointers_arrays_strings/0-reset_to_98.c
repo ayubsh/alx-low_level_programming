51c65013b3a96ca1598a955069db62a781c8c21e
ayubsh <ibrahimayubsheikh@gmail.com>
2022-07-11 04:55:22 -0700

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - sets the input to 98
+ * @n: input to be reseted
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# Pointers, Array and Strings
+
+- Contains tasks for 5 of low level c programing 
