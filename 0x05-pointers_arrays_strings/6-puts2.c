996915a50696a8c7136b532ff4b32cf7a524ef81
ayubsh <ibrahimayubsheikh@gmail.com>
2022-07-11 07:52:42 -0700

    every other string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every other char starting from 0
+ * @str: string to be printed
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int len;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		putchar(str[len]);
+		len += 2;
+	}
+	putchar('\n');
+}
