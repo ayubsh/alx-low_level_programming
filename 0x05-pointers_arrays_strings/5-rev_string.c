8426ec939607a835f65894bd94a2245f4b1f7798
ayubsh <ibrahimayubsheikh@gmail.com>
2022-07-11 07:29:36 -0700

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: string to be reverse
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	len--;
+
+	while (len >= 0)
+	{
+		putchar(s[len]);
+		len--;
+	}
+}
