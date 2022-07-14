901a3795a564353a39e63926452659aa5fe29e03
ayubsh <ibrahimayubsheikh@gmail.com>
2022-07-14 07:42:54 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies string src to dest
+ * @dest: string to be copied to
+ * @src: the string be copied
+ * @n: number of n
+ * Return: return the copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	char *temp;
+
+	temp = dest;
+
+	for (i = 0; i < n; i++)
+	{
+		*dest++ = *src++;
+	}
+	return (temp);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
 #endif
