097621f044003fbb486cfd40647ea6712ef832e6
ayubsh <ibrahimayubsheikh@gmail.com>
2022-07-14 10:16:24 -0700

    encode

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * leet - encodes characters
+ * @s: string to be encoded
+ * Return: encoded string
+ */
+
+char *leet(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == 'a' || s[i] == 'A')
+		{
+			s[i] = 4;
+		}
+
+		if (s[i] == 'e' || s[i] == 'E')
+		{
+			s[i] = 3;
+		}
+
+		if (s[i] == 'o' || s[i] == 'O')
+		{
+			s[i] = 0;
+		}
+
+		if (s[i] == 't' || s[i] == 'T')
+		{
+			s[i] = 7;
+		}
+
+		if (s[i] == 'l' || s[i] == 'L')
+		{
+			s[i] = 1;
+		}
+	}
+
+	return (s);
+}
