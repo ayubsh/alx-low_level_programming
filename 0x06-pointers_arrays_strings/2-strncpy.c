c9dcf22306d28ef765da5d9081703cd18eb13ec9
ayubsh <ibrahimayubsheikh@gmail.com>
2022-07-14 08:20:22 -0700

    copies string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,10 +15,10 @@ char *_strncpy(char *dest, char *src, int n)
 
 	temp = dest;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		*dest++ = *src++;
 	}
-	putchar('\n');
+
 	return (temp);
 }
