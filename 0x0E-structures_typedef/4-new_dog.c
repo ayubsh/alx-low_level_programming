cd7a2807151adb38daef8e28198c36ec15f57117
ayubsh <ibrahimayubsheikh@gmail.com>
2022-08-01 08:37:37 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,13 @@
 #include "dog.h"
 #include <stdio.h>
 
+/**
+ * new_dog - creates new dog struct
+ * @name: name of the dog (ptr)
+ * @age: age of the dog
+ * @owner: owner of the dog (ptr)
+ * Return: returns ptr to new dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *puppy;
