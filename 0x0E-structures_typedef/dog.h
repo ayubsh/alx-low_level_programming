005cf28533005774290b5a74d59185c23cd5297a
ayubsh <ibrahimayubsheikh@gmail.com>
2022-08-01 07:28:36 -0700

    dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog structure
+ * @name: ptr to the name of the dog
+ * @age: age of the dog
+ * @owner: ptr to the owner of the dog
+ * Description: its a structure for dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
