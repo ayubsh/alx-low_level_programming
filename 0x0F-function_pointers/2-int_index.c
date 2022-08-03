3a7397edc42bfcb96033da0917abf8f4fe7c65ca
ayubsh <ibrahimayubsheikh@gmail.com>
2022-08-03 19:44:52 +0300

    searches for interger

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an interger
+ * @size: size of the array
+ * @array: array to be interated on
+ * @cmp: function ptr that is used to compair
+ * Return: returns 1 or -1 or 0 depending on cmp
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (size <= 0)
+{
+return (-1);
+}
+
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+{
+return (i);
+}
+}
+
+return (-1);
+}
