f10820cd99f11132d8224f705e11cf2a42988320
ayubsh <ibrahimayubsheikh@gmail.com>
2022-08-03 19:18:31 +0300

    fun-ptr iterated on array items

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates array then calls function on each item
+ * @size: size of the array
+ * @action: function to be called on each item of the array (ptr)
+ * @array: array to be iterated on
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
