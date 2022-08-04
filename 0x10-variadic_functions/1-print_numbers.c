d5edc0e0c0f5ca49aea4ee1b624f1179ffb5ee79
ayubsh <ibrahimayubsheikh@gmail.com>
2022-08-04 10:36:11 -0700

    checked NULL ptr

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,14 +14,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list list;
 
-	if (separator == NULL)
-		return;
-
 	va_start(list, n);
 
 	for (i = 0; i < n - 1; i++)
 	{
-		printf("%d%s", va_arg(list, unsigned int), separator);
+		if (separator != NULL)
+		{
+			printf("%d%s", va_arg(list, unsigned int), separator);
+		}
+
 	}
 	printf("%d\n", va_arg(list, unsigned int));
 
