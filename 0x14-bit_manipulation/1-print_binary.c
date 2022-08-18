ff729b6b5238d201bf04d74022df18582b7799ca
ayubsh <ibrahimayubsheikh@gmail.com>
2022-08-18 11:20:37 -0700

    decimal in binary form

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,15 +7,12 @@
  */
 void print_binary(unsigned long int n)
 {
-	int c, result;
-
-	for (c = 31; c >= 0; c--)
+	if (n >> 0) 
 	{
-		result = n >> c;
-
-		if (result & 1)
-			printf("1");
-		else
-			printf("0");
+		if (n >> 1)
+			print_binary(n >> 1);
+		putchar((n & 1) + '0');
 	}
+	else
+		putchar('0');
 }
